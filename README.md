ccaececc41f90e13728def2ec0c1adb92704a543
Pen <3045316+GHPen@users.noreply.github.com>
2017-11-23 16:30:48 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,150 @@
+# pdfoutliner
+Command line interface for generating [`pdftk`-style](https://www.pdflabs.com/blog/export-and-import-pdf-bookmarks/) bookmark files in a user-friendly way, and (optionally) outputs a PDF file with the specified outline. 
+
+## Table of Contents
+<!-- MarkdownTOC -->
+
+- [Why](#why)
+- [Sample Usage](#sample-usage)
+- [TOC Format](#toc-format)
+  - [Specifying structure by subheading numbering](#specifying-structure-by-subheading-numbering)
+  - [Specifying structure by indentation](#specifying-structure-by-indentation)
+  - [Keeping PDF flat](#keeping-pdf-flat)
+- [Additional Options](#additional-options)
+- [Dependency](#dependency)
+
+<!-- /MarkdownTOC -->
+
+## Why
+Instead of requiring a TOC file like this, as `pdftk` does
+
+    BookmarkBegin
+    BookmarkTitle: PDF Reference (Version 1.5)
+    BookmarkLevel: 1
+    BookmarkPageNumber: 1
+    BookmarkBegin
+    BookmarkTitle: Contents
+    BookmarkLevel: 2
+    BookmarkPageNumber: 3
+
+To create a PDF file with a structured/nested outline with the script, you only need a TOC file that looks like this: 
+
+    PDF Reference (Version 1.5) 1
+      Contents 3
+
+or perhaps better, this: 
+
+    1 PDF Reference (Version 1.5) 1
+    1.1 Contents 3
+
+
+## Sample Usage
+
+#### With PDF I/O:
+
+    python pdfoutliner.py TOC --input-pdf in.pdf -s START
+
+where 
+
+- `START` is the page in the PDF where p. 1 is supposed to start, and 
+- `TOC` is the path to a table of contents file. 
+
+See section [TOC Format](#toc-format) for details on the syntax. 
+
+#### Writing a pdftk bookmark file only:
+
+    python pdfoutliner.py TOC
+
+For more options, see section [Additional Options](#additional-options), or use
+
+    python pdfoutliner.py -h
+
+## TOC Format
+The default table of contents format is
+
+    1 Heading 1
+    1.2 Subheading 3
+    1.2.3 Subsubheading 5
+
+Each line has a numbering (not necessarily numerical), a title, and a page number, separated by space characters. 
+
+The script will infer that "1 Heading" is level 1, "1.1 Subheading" is level 2, and so on. 
+
+Alternatively, you can [specify the structure by indentation](#specifying-structure-by-indentation), or [keep the PDF flat](#keeping-pdf-flat). 
+
+### Specifying structure by subheading numbering
+This is the default option. As mentioned, the format is
+
+    1 Heading 1
+    1.1 Subheading 3
+    1.1.1 Subsubheading 5
+
+And the script will infer the structure from the numbering. 
+
+If your TOC file looks like 
+
+    1. Heading 1
+    1.1. Subheading 3
+    1.1.1. Subsubheading 5
+
+i.e., has a trailing dot after each numbering, you could specify the style of the heading with `--style 1.2.`
+
+### Specifying structure by indentation
+You could also specify the structure of the outline by indentation with `-d --indentation`, followed by an escaped regex for 1 unit of indentation. 
+
+For example, suppose my TOC looks like
+
+    Heading 1
+      Subheading 3
+        Subsubheading 5
+
+where the unit of indentation is 2 spaces, then use
+    
+    python pdfoutliner.py TOC -d \\s\\s
+
+And the script will infer the structure from the subheading indentations. 
+
+### Keeping PDF flat
+Use `-k --keep-flat` and the script will ignore any numbering or indentations. The output PDF will have a flat, unstructured outline. 
+
+    Heading 1
+    Subheading 3
+    Subsubheading 5
+
+## Additional Options
+    usage: pdfoutliner.py [-h] [-o OUTMARKS] [-d INDENTATION] [-k]
+                          [--style {1.2,1.2.}] [--outpdf OUTPDF] [--inpdf INPDF]
+                          [-s START]
+                          toc
+
+    optional arguments:
+      -h, --help            show this help message and exit
+
+    bookmark I/O:
+      toc                   path to TOC file
+      -o OUTMARKS, --outmarks OUTMARKS
+                            name for pdftk bookmarks file. default is original toc
+                            name + "_outlined"
+
+    bookmark structure:
+      if both -d and -k are specified, -d will take precedence over -k
+
+      -d INDENTATION, --indentation INDENTATION
+                            escaped regex for 1 unit of indentation
+      -k, --keep-flat       keep outline flat
+      --style {1.2,1.2.}    heading style. with or without a trailing dot. default
+                            "1.2", i.e., no trailing dot
+
+    PDF I/O:
+      --outpdf OUTPDF       path to output PDF file. default is input pdf name +
+                            "_outlined.pdf" in input PDF's directory
+      --inpdf INPDF         path to input PDF file
+      -s START, --start START
+                            page in the pdf document where page 1 is. default 1
+
+## Dependency
+
+- [pdftk](https://www.pdflabs.com/tools/pdftk-server/)
+    - on macOS 10.11+, use the build [here](https://www.pdflabs.com/tools/pdftk-the-pdf-toolkit/pdftk_server-2.02-mac_osx-10.11-setup.pkg)
+- (optional) [Tabula](http://tabula.technology/)
+    - for extracting a usable TOC from PDF files (along with some additional [regex golfing](https://xkcd.com/1313/))
---
+++ pdfoutliner.py
@@ -0,0 +1,141 @@
+import os
+import re
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser()
+    # bookmark I/O
+    group_bookmark = parser.add_argument_group("bookmark I/O")
+    group_bookmark.add_argument("toc", help="path to TOC file", type=str)
+    group_bookmark.add_argument("-o", "--outmarks", 
+        help="name for pdftk bookmarks file. " + 
+        "default is original toc name + \"_outlined\"", type=str)
+    # bookmark structure
+    group_structure = parser.add_argument_group("bookmark structure",
+        "if both -d and -k are specified, -d will take precedence over -k")
+    group_structure.add_argument("-d", "--indentation", 
+        help="escaped regex for 1 unit of indentation", type=str)
+    group_structure.add_argument("-k", "--keep-flat", 
+        help="keep outline flat", 
+        action="store_true")
+    group_structure.add_argument("--style",
+        choices=["1.2", "1.2."], default="1.2",
+        help="heading style. with or without a trailing dot. "+
+        "default \"1.2\", i.e., no trailing dot", type=str)
+    # pdf I/O
+    group_pdf = parser.add_argument_group("PDF I/O")
+    group_pdf.add_argument("--outpdf", help="path to output PDF file. " +
+        "default is input pdf name + \"_outlined.pdf\" in input PDF's directory", type=str)
+    group_pdf.add_argument("--inpdf", help="path to input PDF file", type=str)
+    group_pdf.add_argument("-s", "--start", default=1,
+        help="page in the pdf document where page 1 is. default 1", 
+        type=int)
+
+    args = parser.parse_args()
+    start = args.start - 1
+    if args.outmarks:
+        marks_name = args.outmarks
+    else:
+        toc_name_ext = os.path.splitext(os.path.basename(args.toc))
+        toc_name, toc_ext = toc_name_ext[0], toc_name_ext[1]
+        marks_name = toc_name + "_bookmarks" + toc_ext
+    toc_dir = os.path.dirname(args.toc)
+    if toc_dir:
+        marks_path = os.path.join(toc_dir, marks_name)
+    else:
+        marks_path = marks_name
+    with open(args.toc, "r") as infile:
+        with open(marks_path, "w") as outfile:
+            line_num = 1
+            if args.indentation:
+                indentation = args.indentation
+                # the pattern for 1 unit of indentation
+                ind_unit_pattern = re.compile(indentation)
+                # the composite pattern
+                # e.g., "^((\s\s)+)" if indentation == "\s\s"
+                ind_comp_pattern = re.compile("^((" + indentation + ")+)")
+            for line in infile:
+                chunk = re.match(r'(.*\s)(\d+)', line)
+                if not chunk:
+                    parser.error(
+                        "incorrect formatting on line {}: \n {}".format(
+                            str(line_num), line))
+                title = chunk.group(1)
+                page = int(chunk.group(2)) + start
+                # infer structure from indentation
+                if args.indentation:
+                    indented = re.search(ind_comp_pattern, title)
+                    if not indented:
+                        level = 1
+                    else:
+                        indented = indented.group(1)
+                        level = len(re.findall(ind_unit_pattern, indented)) + 1
+                    # strip title of indentation
+                    title = re.sub(ind_comp_pattern, "", title)
+                elif args.keep-flat: 
+                    level = 1
+                    if not args.inpdf:
+                        parser.error("-k --keep-flat is to be used with --inpdf")
+                else:
+                    # infer structure from numbering by counting the number of 
+                    # dots. e.g., "1.2.3 Chapter Title" is level 3 
+                    #
+                    # take everything before the first space. accomodates 
+                    # appendices numberings like A.1.2
+
+                    # integrity check
+                    e_temp = "subheading not in the style of \"{}\" on line {}: \n {}"
+                    e_msg = e_temp.format(args.style, line_num, line)
+                    if args.style == "1.2":
+                        if not re.search(r'^\s*(\w|\.)*\w+\s', line):
+                            parser.error(e_msg)
+                    if args.style == "1.2.":
+                        if not re.search(r'^\s*(\w|\.)+\.\s', line):
+                            parser.error(e_msg)
+                    indented_search = re.search(r'^(.*)\s?', line)
+                    level_offset = 2 - len(re.findall(r'\.', args.style))
+                    if indented_search:
+                        indented = indented_search.group(1)
+                        level = len(re.findall(r'\.', indented)) + level_offset
+                    else:
+                        level = 1
+                outfile.writelines(["BookmarkBegin\n",
+                                    "BookmarkTitle: {}\n".format(title), 
+                                    "BookmarkLevel: {}\n".format(level), 
+                                    "BookmarkPageNumber: {}\n".format(page)])
+                line_num += 1
+    if args.outpdf and not args.inpdf:
+        parser.error("input PDF not specified")
+    # call pdftk
+    if args.inpdf: 
+        if args.outpdf:
+            outpdf_dir = os.path.dirname(args.outpdf)
+            # if a directory is specified for the output pdf
+            if outpdf_dir:
+                pdf_out_path = args.outpdf
+            # only output name is specified
+            # defer dir to args.inpdf
+            else:
+                pdf_out_name = args.outpdf
+        # default pdf outfile
+        try:
+            pdf_out_name
+        # pdf_out_name hasn't been previously defined
+        except NameError:
+            pdf_out_name = os.path.splitext(args.inpdf)[0] + "_outlined.pdf"
+        try:
+            not pdf_out_path
+        # pdf_out_path hasn't been previously defined
+        except NameError:
+            dir_path = os.path.dirname(args.inpdf)
+            if dir_path:
+                pdf_out_path = os.path.join(dir_path, pdf_out_name)
+            else:
+                pdf_out_path = pdf_out_name
+        os.system("pdftk {} update_info {} output {}".format(args.inpdf, 
+            marks_path, pdf_out_path))
+        # delete intermediary bookmark file
+        os.remove(marks_path)
+
+if __name__ == "__main__":
+    main()
