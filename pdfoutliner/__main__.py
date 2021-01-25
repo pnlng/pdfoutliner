bb78b9bb1d5bd3492b57b36429ceafa10b413c82
GHPen <3045316+GHPen@users.noreply.github.com>
2021-01-24 21:29:30 -0500

    Use sentence case for comments

--- pdfoutliner/__main__.py
+++ pdfoutliner/__main__.py
@@ -5,13 +5,13 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    # bookmark I/O
+    # Bookmark I/O
     group_bookmark = parser.add_argument_group("bookmark I/O")
     group_bookmark.add_argument("toc", help="path to TOC file", type=str)
     group_bookmark.add_argument("-o", "--outmarks", 
         help="name for pdftk bookmarks file. " + 
         "default is original toc name + \"_outlined\"", type=str)
-    # bookmark structure
+    # Bookmark structure
     group_structure = parser.add_argument_group("bookmark structure",
         "if both -d and -k are specified, -d will take precedence over -k")
     group_structure.add_argument("-d", "--indentation", 
@@ -23,7 +23,7 @@ def main():
         choices=["1.2", "1.2."], default="1.2",
         help="heading style. with or without a trailing dot. "+
         "default \"1.2\", i.e., no trailing dot", type=str)
-    # pdf I/O
+    # PDF I/O
     group_pdf = parser.add_argument_group("PDF I/O")
     group_pdf.add_argument("--outpdf", help="path to output PDF file. " +
         "default is input pdf name + \"_outlined.pdf\" in input PDF's directory", type=str)
@@ -52,13 +52,13 @@ def main():
             line_num = 1
             if args.indentation:
                 indentation = args.indentation
-                # the pattern for 1 unit of indentation
+                # The pattern for 1 unit of indentation
                 ind_unit_pattern = re.compile(indentation)
-                # the composite pattern
+                # The composite pattern,
                 # e.g., "^((\\s\\s)+)" if indentation == "\\s\\s"
                 ind_comp_pattern = re.compile("^((" + indentation + ")+)")
             for line in infile:
-                # replace a few common non-ascii characters
+                # Replace a few common non-ascii characters
                 line = str.replace(line, "’", "'")
                 line = str.replace(line, "“", "\"")
                 line = str.replace(line, "“", "\"")
@@ -73,7 +73,7 @@ def main():
                     parser.error(
                         "page number out of range on line {} \n {}".format(
                         str(line_num), line))
-                # infer structure from indentation
+                # Infer structure from indentation
                 if args.indentation:
                     indented = re.search(ind_comp_pattern, title)
                     if not indented:
@@ -81,20 +81,20 @@ def main():
                     else:
                         indented = indented.group(1)
                         level = len(re.findall(ind_unit_pattern, indented)) + 1
-                    # strip title of indentation
+                    # Strip title of indentation
                     title = re.sub(ind_comp_pattern, "", title)
                 elif args.keepflat: 
                     level = 1
                     if not args.inpdf:
                         parser.error("-k --keepflat is to be used with --inpdf")
                 else:
-                    # infer structure from numbering by counting the number of 
+                    # Infer structure from numbering by counting the number of 
                     # dots. e.g., "1.2.3 Chapter Title" is level 3 
                     #
-                    # take everything before the first space. accomodates 
+                    # Take everything before the first space. Accomodates 
                     # appendices numberings like A.1.2
 
-                    # integrity check
+                    # Integrity check
                     e_temp = "subheading not in the style of \"{}\" on line {}: \n {}"
                     e_msg = e_temp.format(args.style, line_num, line)
                     if args.style == "1.2":
@@ -117,33 +117,33 @@ def main():
                 line_num += 1
     if args.outpdf and not args.inpdf:
         parser.error("input PDF not specified")
-    # call pdftk
+    # Call pdftk
     if args.inpdf: 
         if args.outpdf:
             outpdf_dir = os.path.dirname(args.outpdf)
-            # if a directory is specified for the output pdf
+            # If a directory is specified for the output pdf
             if outpdf_dir:
                 pdf_out_path = args.outpdf
-            # only output name is specified
-            # defer dir to args.inpdf
+            # Only output name is specified
+            # Defer dir to args.inpdf
             else:
                 pdf_out_name = args.outpdf
-        # default pdf outfile
+        # Default pdf outfile
         try:
             pdf_out_name
-        # pdf_out_name hasn't been previously defined
+        # `pdf_out_name` hasn't been previously defined
         except NameError:
             pdf_out_name = os.path.splitext(args.inpdf)[0] + "_outlined.pdf"
         try:
             not pdf_out_path
-        # pdf_out_path hasn't been previously defined
+        # `pdf_out_path` hasn't been previously defined
         except NameError:
             dir_path = os.path.dirname(args.inpdf)
             if dir_path:
                 pdf_out_path = os.path.join(dir_path, pdf_out_name)
             else:
                 pdf_out_path = pdf_out_name
-        # update_info hasn't been previously defined
+        # `update_info` hasn't been previously defined
         if args.utf8:
             update_info = "update_info_utf8"
         else:
@@ -151,7 +151,7 @@ def main():
 
         os.system("pdftk {} {} {} output {}".format(args.inpdf, 
             update_info, marks_path, pdf_out_path))
-        # delete intermediary bookmark file
+        # Delete intermediary bookmark file
         os.remove(marks_path)
 
 if __name__ == "__main__":
