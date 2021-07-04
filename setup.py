21098c9d1077be9e26cb7a065570f05fe0f1e8c7
GHPen <3045316+GHPen@users.noreply.github.com>
2021-07-04 14:43:13 -0400

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="pdfoutliner",
-      version="0.0.3",
-      author="GHPen",
+      version="0.0.4",
+      author="pnlng",
       description="Command line interface for generating pdftk-style bookmark files in a user-friendly way, and (optionally) outputs a PDF file with the specified outline.",
       long_description=long_description,
       long_description_content_type="text/markdown",
-      url="https://github.com/GHPen/pdfoutliner",
+      url="https://github.com/pnlng/pdfoutliner",
       packages=["pdfoutliner"],
       classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
       ],
       entry_points={
           "console_scripts": [
               "pdfoutliner = pdfoutliner.__main__:main"
           ]
       },
-        python_requires='>=3.5'
+      python_requires='>=3.5'
       )
